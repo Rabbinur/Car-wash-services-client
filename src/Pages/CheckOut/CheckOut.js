4bfae732e5e85cfed1973dfe8402632390356ce0
Md. Rabbinur Muktar <rabbinur.cse.bubt@gmail.com>
2022-11-16 15:22:47 +0600

    checkout from create and order object create

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,35 +1,71 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title } = service;
+  const { user } = useContext(AuthContext);
+  const { _id, title, price } = service;
+
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const email = user?.email || "unregistered";
+    const phone = form.phone.value;
+    const message = form.message.value;
+
+    const order = {
+      service: _id,
+      serviceName: title,
+      price,
+      customer: name,
+      phone,
+      message,
+    };
+  };
+
   return (
     <div>
-      <h1>This is CheckOut page</h1>
-      <form>
+      <form onSubmit={handlePlaceOrder}>
+        <div className="text-center">
+          <h2 className="text-4xl">You are about order : {title}</h2>
+          <h4 className="text-3xl">Product Price : {price}</h4>
+        </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <input
+            name="firstName"
             type="text"
-            placeholder="Type here"
-            className="input input-bordered input-secondary w-full max-w-xs"
+            placeholder="First Name"
+            className="input input-bordered input-secondary w-full "
           />
           <input
+            name="lastName"
             type="text"
-            placeholder="Type here"
-            className="input input-bordered input-secondary w-full max-w-xs"
+            placeholder="Last Name"
+            className="input input-bordered input-secondary w-full"
           />
           <input
+            name="phone"
             type="text"
-            placeholder="Type here"
-            className="input input-bordered input-secondary w-full max-w-xs"
+            placeholder="Your Phone"
+            className="input input-bordered input-secondary w-full "
           />
           <input
+            name="email"
             type="text"
-            placeholder="Type here"
-            className="input input-bordered input-secondary w-full max-w-xs"
+            placeholder="Your email"
+            defaultValue={user?.email}
+            className="input input-bordered input-secondary w-full "
+            readOnly
           />
         </div>
+        <textarea
+          name="message"
+          className="textarea textarea-secondary h-24 w-full mt-4"
+          placeholder="your message"
+        ></textarea>
+        <input className="btn" type="submit" value="Place your Order" />
       </form>
     </div>
   );
