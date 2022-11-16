9c1d1f770054380894ad715e99fb815e5853d165
Md. Rabbinur Muktar <rabbinur.cse.bubt@gmail.com>
2022-11-16 14:23:59 +0600

    set up localhost for services data

--- src/Others/Term&Condition/TermsCondition.js
+++ src/Others/Term&Condition/TermsCondition.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useTitle from "../../Hooks/UseTitle";
 
 const TermsCondition = () => {
+    useTitle("Terms");
   return (
     <div>
       <h3>Here is our Terms and COnditions</h3>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -7,7 +7,7 @@ const Services = () => {
   //load sevices data
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
